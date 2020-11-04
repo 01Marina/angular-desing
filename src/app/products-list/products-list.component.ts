2a35ab6d6548b12a52ccccd980025b297118d7cf
Marina <marinacaseres1997@hotmail.com>
2020-11-03 22:08:36 -0300

    Agrego interfaz y derectivas

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from './products';
 
 @Component({
   selector: 'app-products-list',
@@ -7,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  products ={
-    "product": "Cuadro",
-    "description": "Decoración de interiores",
-    "price":1500,
-    "stock":43,
-    "image": "assets/img/cuadro.png"
-  }
+  products: Products[] = [
+    {
+      product: "Cuadro",
+      description: "Decoración de interiores",
+      price:1500,
+      stock:43,
+      image: "assets/img/cuadro.png",
+      settlement: false,
+    },
+    {
+      product: "Agenda",
+      description: "Papelera de oficina",
+      price:564,
+      stock:0,
+      image: "assets/img/agenda-negra-frente.jpg",
+      settlement: true,
+    },
+    {
+      product: "cuadro",
+      description: "Decoración de interiores",
+      price:2530,
+      stock:21,
+      image: "assets/img/cuadro-simpe.png",
+      settlement: false,
+    },
+  ];
 
   constructor() { }
 
---
+++ src/app/products-list/products.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    product: string;
+    description: string;
+    price:number;
+    stock:number;
+    image:string;
+    settlement: boolean;
+}
